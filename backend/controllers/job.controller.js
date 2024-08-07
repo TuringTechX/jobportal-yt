fcc377416798e9c06fc6301040a8832ccd40ac79
coo <157929526+paycoo-droid@users.noreply.github.com>
2024-08-07 11:21:14 +0000

    modified:   backend/controllers/application.controller.js 	modified:   backend/controllers/job.controller.js 	modified:   frontend/src/components/HeroSection.jsx 	modified:   frontend/src/components/shared/Footer.jsx

--- backend/controllers/job.controller.js
+++ backend/controllers/job.controller.js
@@ -1,6 +1,6 @@
 import { Job } from "../models/job.model.js";
 
-// admin post krega job
+// admin post job
 export const postJob = async (req, res) => {
     try {
         const { title, description, requirements, salary, location, jobType, experience, position, companyId } = req.body;
@@ -78,7 +78,7 @@ export const getJobById = async (req, res) => {
         console.log(error);
     }
 }
-// admin kitne job create kra hai abhi tk
+// admin job create kra hai abhi tk
 export const getAdminJobs = async (req, res) => {
     try {
         const adminId = req.id;
